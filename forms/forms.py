74a0dabe658210f0490b9cc082aa5008a9c220dc
lche338 <63653233+lche338@users.noreply.github.com>
2020-04-25 00:37:26 +1000

    Update forms.py

--- forms/forms.py
+++ forms/forms.py
@@ -16,16 +16,16 @@ class SafetyQuestionnaireForm(FlaskForm):
     grandparent_name = StringField('Please enter your grandparent name.',validators=[DataRequired()])
 
     grandparent_gender = SelectField('Selet the gender of your grandparent:',
-                    choices =[('Male','Male'),('Female','Female')],validators=[DataRequired()])
+                    choices =[(' ',' '),('Male','Male'),('Female','Female')],validators=[DataRequired()])
                            
     grandparent_age = SelectField('Selet the age of your grandparents:',
-                    choices =[('65-74','65-74'),('75-84','75-84'),('85-94','85-94'),('95+','95+'),('none of above or not sure','none of above or not sure')],validators=[DataRequired()])
+                    choices =[(' ',' '),('65-74','65-74'),('75-84','75-84'),('85-94','85-94'),('95+','95+'),('none of above or not sure','none of above or not sure')],validators=[DataRequired()])
 
     adequate_light = SelectField('Does the house where your grandparent lived have adequate lighting in the following areas? (mutiple choice)',
-                        choices=[('Bedroom','Bedroom'),('Kitchen','Kitchen'),('Living Room','Living Room'),('Walkways','Walkways'),('Laundry','Laundry'),('Bathroom','Bathroom'),('Stairs','Stairs'),('Driveway and garage','Driveway and garage')], validators=[DataRequired()])
+                        choices=[(' ',' '),('Bedroom','Bedroom'),('Kitchen','Kitchen'),('Living Room','Living Room'),('Walkways','Walkways'),('Laundry','Laundry'),('Bathroom','Bathroom'),('Stairs','Stairs'),('Driveway and garage','Driveway and garage')], validators=[DataRequired()])
 
     night_light = SelectField('Does the house where your grandparent lived have night-lights or signs giving directions to the following areas? (mutiple choice)',
-                        choices=[('Bedroom','Bedroom'),('Kitchen','Kitchen'),('Living Room','Living Room'),('Walkways','Walkways'),('Laundry','Laundry'),('Bathroom','Bathroom'),('Stairs','Stairs'),('Driveway and garage','Driveway and garage')], validators=[DataRequired()])                      
+                        choices=[(' ',' '),('Bedroom','Bedroom'),('Kitchen','Kitchen'),('Living Room','Living Room'),('Walkways','Walkways'),('Laundry','Laundry'),('Bathroom','Bathroom'),('Stairs','Stairs'),('Driveway and garage','Driveway and garage')], validators=[DataRequired()])                      
                            
     clear_path = RadioField('Do you think there is a clear, unobstructed path through the room?',
                     choices =[('Yes','Yes'),('No','No')],validators=[DataRequired()])
